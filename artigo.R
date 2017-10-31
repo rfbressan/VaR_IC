2ce748259131e03fa22bd200576c233234fd624e
Rafael Bressan <rfbressan@gmail.com>
2017-10-31 14:55:05 -0200

    Inicio do modelo

--- artigo.R
+++ artigo.R
@@ -60,16 +60,22 @@ assets.tbl <- assets.tbl %>%
          assim = map_dbl(ts, ~skewness(.x)),
          curtose = map_dbl(ts, ~kurtosis(.x)),
          jarquebera = map(ts, ~jarqueberaTest(as.timeSeries(.x))),
-         ljungbox = map(ts, ~Box.test(.x, lag = 10, type = "Ljung-Box"))) %>% 
+         ljungbox = map(ts, ~Box.test(.x, lag = 10, type = "Ljung-Box")),
+         ljungbox2 = map(ts, ~Box.test(.x^2, lag = 10, type = "Ljung-Box"))) %>% 
   mutate(jbstat = map_dbl(jarquebera, ~as.numeric(.x@test$statistic)),
          jbpvalue = map_dbl(jarquebera, ~as.numeric(.x@test$p.value)),
          lbstat = map_dbl(ljungbox, ~as.numeric(.x$statistic)),
          lbpvalue = map_dbl(ljungbox, ~as.numeric(.x$p.value)),
+         lb2stat = map_dbl(ljungbox2, ~as.numeric(.x$statistic)),
+         lb2pvalue = map_dbl(ljungbox2, ~as.numeric(.x$p.value)),
          nobs = map_int(ts, ~as.integer(length(.x))))
 
 # Transpor esta tabela para apresentacao
-df.descritivas <- data.frame(t(assets.tbl[,-c(1, 2, 3, 11, 12)]))
+df.descritivas <- data.frame(t(assets.tbl[,-c(1, 2, 3, 11, 12, 13)]))
 colnames(df.descritivas) <- assets.tbl$id_name
+row.names(df.descritivas) <- c("Média", "Mediana", "Máximo", "Mínimo", "Desvp", "Assimetria", "Curtose",
+                               "Jarque-Bera", "p-valor", "Q(10)", "p-valo", "Q^2(10)", "p-val",
+                               "N.obs")
 # Cria o xtable
 tab1 <- xtable(df.descritivas, caption = "Estatísticas descritivas dos retornos 
                (amostra completa de 31/08/2005 a 30/08/2017.",
@@ -88,10 +94,16 @@ list.plot <- lapply(seq_along(assets.tbl$indice),
 grid.arrange(grobs = list.plot)
 
 # Teste para graficos QQ normal
-qqnormPlot(as.timeSeries(assets.tbl$ts[[1]]), title = FALSE, main = "Ibov")
-
+op <- par(mfrow = c(3,2))
+for(i in 1:dim(assets.tbl)[1]){
+  qqnormPlot(as.timeSeries(assets.tbl$ts[[i]]), labels = FALSE, title = FALSE, mtext = FALSE,
+             main = assets.tbl$id_name[[i]],
+             xlab = "Normal",
+             ylab = "Amostra")
+}
+par(op)
 ###################################################################################
-## Lembrando, o modelo das perdas é ARMA(1,1) e a volatilidade é GARCH(1,1)
+## Lembrando, o modelo das perdas é ARMA(1,1) e a volatilidade é EGARCH(1,1)
 ## L_t=mu_t+e_t      mu_t=mu+phi*mu_t-1+theta*e_t-1
 ## e_t=sigma_t*z_t   sigma^2_t=alpha0+alpha1*e^2_t-1+beta*sigma^2t-1
 ## O modelo retorno 5 parametros:
@@ -105,6 +117,7 @@ qqnormPlot(as.timeSeries(assets.tbl$ts[[1]]), title = FALSE, main = "Ibov")
 
 # Lfit1 usa método de fGarch
 # Lfit2 usa método de rugarch
+# LEMBRAR: ts contem os retornos e não as perdas!
 Lfit1 <- garchFit(formula = ~arma(1,1)+garch(1,1), 
                   data = losses[paste0("/", end),],
                   include.mean = TRUE, 
