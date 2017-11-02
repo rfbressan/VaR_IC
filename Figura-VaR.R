110a9855cac8b1aa276269d1dadbd3dbc2f87628
Rafael Bressan <rfbressan@gmail.com>
2017-11-02 07:44:38 -0200

    Figura VaR

---
+++ Figura-VaR.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+x <- seq(-3, 3, by = 0.05)
+y <- dnorm(x)
+df <- data.frame(x, y)
+p <- ggplot(df, aes(x, y)) + 
+  geom_area(data = subset(df, x < -1), fill = "tomato") +
+  #geom_area(data = subset(df, x < -1.96), fill = "red") +
+  #geom_area(data = subset(df, x > 1), fill = "lightgreen") +
+  #geom_area(data = subset(df, x > 1.96), fill = "green") +
+  geom_line()+
+  labs(x = "", y = "")
+
+p+annotate("text", x = c(-1, -1.5), y = c(-0.02, 0.05), label = c("VaR", "~alpha"), parse = TRUE, size = 6)
+
+p
